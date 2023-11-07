f8f6fb699d4d8716806cc10eefc1b96ea45ba1d6
joy777park <joy777park@gmail.com>
2023-11-07 23:24:53 +0800

    sql scripts

---
+++ CustomerManagementApi/sql/Customers.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE CustomerInfo (
+    Id int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    EMail varchar(100) not null,
+    Password varchar(30) not null,
+    Name nvarchar(30) not null,
+    Age int null,
+    Gender nvarchar(1) not null,
+    AreaName nvarchar(20) not null,
+    CityName nvarchar(20) not null,
+    INDEX IX_CustomerInfo NONCLUSTERED (Name, CityName)
+)
---
+++ CustomerManagementApi/sql/region.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE RegionInfo (
+    AreaName nvarchar(20) NOT NULL,
+    CityName nvarchar(20) NOT NULL,
+    INDEX IX_RegionInfo NONCLUSTERED (AreaName, CityName)
+)
