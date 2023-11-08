c9eb67adf6af611b5e266585baeb1fe759f6e46d
joy777park <joy777park@gmail.com>
2023-11-08 23:48:38 +0800

    單元測試

---
+++ CustomerManagementApiTests/Controllers/WeatherForecastControllerTest.cs
@@ -0,0 +1,34 @@
+﻿using CustomerManagementApi;
+using CustomerManagementApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+
+namespace CustomerManagementApiTests.Controllers
+{
+    [TestClass]
+    public class WeatherForecastControllerTest
+    {
+        private ILogger<WeatherForecastController> _logger;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _logger = Substitute.For<ILogger<WeatherForecastController>>();
+        }
+
+        [TestMethod]
+        public void Test_Returns()
+        {
+            // Arrange
+            var controller = new WeatherForecastController(_logger);
+
+            // Act
+            IEnumerable<WeatherForecast> result = controller.Get();
+
+            // Assert
+            var viewResult = result;
+            Assert.IsInstanceOfType(viewResult, typeof(IEnumerable<WeatherForecast>));
+        }
+    }
+}
